88c32ce7d28caf2f70286743191ffbfdbf8ec3e6
liskotlas <lis_kotlas@mail.ru>
2019-11-16 22:10:36 +0300

    3

--- src/ru/progwards/java1/lessons/classes/ComplexNum.java
+++ src/ru/progwards/java1/lessons/classes/ComplexNum.java
@@ -23,7 +23,7 @@ public class ComplexNum {
     }
 
     public ComplexNum mul(ComplexNum num1, ComplexNum num2){
-        return new ComplexNum(num1.a * num2.b - num1.b * num2.b, num1.b * num2.a + num1.a * num2.b);
+        return new ComplexNum(num1.a * num2.a - num1.b * num2.b, num1.b * num2.a + num1.a * num2.b);
     }
 
     public ComplexNum div(ComplexNum num1, ComplexNum num2){
