7f75221b8a57dc4aa953bdf5d763a984ea1e935d
liskotlas <lis_kotlas@mail.ru>
2019-12-03 22:23:59 +0300

    3

--- src/ru/progwards/java1/lessons/interfaces/ArraySort.java
+++ src/ru/progwards/java1/lessons/interfaces/ArraySort.java
@@ -1,22 +1,21 @@
 package ru.progwards.java1.lessons.interfaces;
 
-import org.checkerframework.checker.units.qual.A;
-
 import java.util.Arrays;
 
 public class ArraySort{
     int tmp;
-//    public static void sort(Comparable<Animal>[] a){
-    public void sort (Animal[] a){
+    public static void sort(Comparable<Animal>[] a){
+        Comparable<Animal> tmp;
+//    public void sort (Animal[] a){
 
            for (int f = 0; f < a.length; f++){
             String s =a[f].toString();
             for (int i = f + 1; i < a.length; i++){
 //                if (a [f] > a [i]){
                 if (a[f].compareTo ((Animal)a[i]) < 0) {
-//                    tmp = a [f];
+                    tmp = a [f];
                     a[f] = a[i];
-//                    a [i] = tmp;
+                    a [i] = tmp;
                 }
                 }
             }
