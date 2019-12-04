68d9fea36435412d2e59330bb84d6a02e5d07cd8
liskotlas <lis_kotlas@mail.ru>
2019-12-04 20:24:07 +0300

    3

--- src/ru/progwards/java1/lessons/interfaces/Animal.java
+++ src/ru/progwards/java1/lessons/interfaces/Animal.java
@@ -1,95 +1,95 @@
-package ru.progwards.java1.lessons.interfaces;
-
-import java.util.Objects;
-
-public class Animal implements Comparable<Animal> {
-    double weight;
-
-    @Override
-    public int compareTo(Animal animal) {
-        if (Animal.this.weight == animal.weight){
-            return 0;
-        }
-        if (Animal.this.weight < animal.weight) {
-            return -1;
-        }
-        return 1;
-    }
-
-
-    enum AnimalKind {
-        ANIMAL,
-        COW,
-        HAMSTER,
-        DUCK
-    }
-
-    enum FoodKind {
-        UNKNOWN,
-        HAY,
-        CORN
-    }
-
-    public double getFood1kgPrice(){
-        switch (getFoodKind()){
-            case UNKNOWN:
-                return 0;
-            case HAY:
-                return 20;
-            case CORN:
-                return 50;
-        }
-        return 0;
-    }
-
-    public double getFoodPrice(){
-        return calculateFoodWeight() * getFood1kgPrice();
-    }
-
-    public int compareFoodPrice(Animal animal){
-       return Double.compare(this.getFoodPrice(), animal.getFoodPrice());
-    }
-
-    public AnimalKind getKind(){
-            return AnimalKind.ANIMAL;
-    }
-
-    public FoodKind getFoodKind() {
-        return FoodKind.UNKNOWN;
-    }
-
-    public String toString() {
-        return "I am " + getKind() + ", eat " + getFoodKind() + calculateFoodWeight();
-    }
-
-    public double getWeight(){
-        return 1;
-    }
-
-    public Animal(double Weight){
-        this.weight = Weight;
-    }
-
-    public double getFoodCoeff(){
-        return 0.02;
-    }
-
-    public double calculateFoodWeight(){
-        return weight * getFoodCoeff();
-    }
-
-    @Override
-    public boolean equals(Object anObject) {
-        if (this == anObject) return true;
-        if (anObject == null || getClass() != anObject.getClass()) return false;
-        Animal animal = (Animal) anObject;
-        return Double.compare(animal.weight, weight) == 0;
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(weight);
-    }
-}
-
-
+//package ru.progwards.java1.lessons.interfaces;
+//
+//import java.util.Objects;
+//
+//public class Animal implements Comparable<Animal> {
+//    double weight;
+//
+//    @Override
+//    public int compareTo(Animal animal) {
+//        if (Animal.this.weight == animal.weight){
+//            return 0;
+//        }
+//        if (Animal.this.weight < animal.weight) {
+//            return -1;
+//        }
+//        return 1;
+//    }
+//
+//
+//    enum AnimalKind {
+//        ANIMAL,
+//        COW,
+//        HAMSTER,
+//        DUCK
+//    }
+//
+//    enum FoodKind {
+//        UNKNOWN,
+//        HAY,
+//        CORN
+//    }
+//
+//    public double getFood1kgPrice(){
+//        switch (getFoodKind()){
+//            case UNKNOWN:
+//                return 0;
+//            case HAY:
+//                return 20;
+//            case CORN:
+//                return 50;
+//        }
+//        return 0;
+//    }
+//
+//    public double getFoodPrice(){
+//        return calculateFoodWeight() * getFood1kgPrice();
+//    }
+//
+//    public int compareFoodPrice(Animal animal){
+//       return Double.compare(this.getFoodPrice(), animal.getFoodPrice());
+//    }
+//
+//    public AnimalKind getKind(){
+//            return AnimalKind.ANIMAL;
+//    }
+//
+//    public FoodKind getFoodKind() {
+//        return FoodKind.UNKNOWN;
+//    }
+//
+//    public String toString() {
+//        return "I am " + getKind() + ", eat " + getFoodKind() + calculateFoodWeight();
+//    }
+//
+//    public double getWeight(){
+//        return 1;
+//    }
+//
+//    public Animal(double Weight){
+//        this.weight = Weight;
+//    }
+//
+//    public double getFoodCoeff(){
+//        return 0.02;
+//    }
+//
+//    public double calculateFoodWeight(){
+//        return weight * getFoodCoeff();
+//    }
+//
+//    @Override
+//    public boolean equals(Object anObject) {
+//        if (this == anObject) return true;
+//        if (anObject == null || getClass() != anObject.getClass()) return false;
+//        Animal animal = (Animal) anObject;
+//        return Double.compare(animal.weight, weight) == 0;
+//    }
+//
+//    @Override
+//    public int hashCode() {
+//        return Objects.hash(weight);
+//    }
+//}
+//
+//
--- src/ru/progwards/java1/lessons/interfaces/Cow.java
+++ src/ru/progwards/java1/lessons/interfaces/Cow.java
@@ -1,25 +1,25 @@
-package ru.progwards.java1.lessons.interfaces;
-
-
-
-public class Cow extends Animal{
-
-    public Cow(double weight){
-        super(weight);
-    }
-
-    @Override
-    public AnimalKind getKind(){
-        return AnimalKind.COW;
-    }
-
-    @Override
-    public FoodKind getFoodKind() {
-        return FoodKind.HAY;
-    }
-
-    @Override
-    public double getFoodCoeff(){
-        return 0.05;
-    }
-}
+//package ru.progwards.java1.lessons.interfaces;
+//
+//
+//
+//public class Cow extends Animal{
+//
+//    public Cow(double weight){
+//        super(weight);
+//    }
+//
+//    @Override
+//    public AnimalKind getKind(){
+//        return AnimalKind.COW;
+//    }
+//
+//    @Override
+//    public FoodKind getFoodKind() {
+//        return FoodKind.HAY;
+//    }
+//
+//    @Override
+//    public double getFoodCoeff(){
+//        return 0.05;
+//    }
+//}
