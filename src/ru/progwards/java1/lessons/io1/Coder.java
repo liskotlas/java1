0dc6b2a3958ecaa66a1b2490b875393b7786e497
liskotlas <lis_kotlas@mail.ru>
2019-12-15 23:29:52 +0300

    3

--- src/ru/progwards/java1/lessons/io1/Coder.java
+++ src/ru/progwards/java1/lessons/io1/Coder.java
@@ -3,6 +3,7 @@ package ru.progwards.java1.lessons.io1;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Coder {
@@ -12,30 +13,34 @@ public class Coder {
         try {
             FileReader reader = new FileReader(inFileName);
             FileWriter writer = new FileWriter(outFileName);
-            try {
-                Scanner scanner = new Scanner(reader);
-                char[] res;
-                for (int i = 0; scanner.hasNextLine(); i++) {
-                    res = scanner.nextLine().toCharArray();
-                    for (int f = 0; f < res.length; f++) {
-                        try {
-                            writer.write(code[(int) res[f]]);
-                            writer.write("\n");
-                        } finally {
-                            writer.close();
-                        }
-                    }
+            Scanner scanner = new Scanner(reader);
+            char[] res;
+            String s = "";
+            for (int i = 0; scanner.hasNextLine(); i++) {
+                res = scanner.nextLine().toCharArray();
+                System.out.println(res.length);
+                System.out.println(Arrays.toString(res));
+                System.out.println(((int) res[1]));
+                for (int f = 0; f < res.length; f++) {
+                    s += code[((int) res[f])];
+                }
+                try {
+                    writer.write(s);
+                    writer.write("\n");
+                    s = "";
+                } catch (IOException e) {
+                    System.out.println(e.getMessage());
+                    ;
                 }
             }
-            finally {
-                reader.close();
-            }
+            reader.close();
+            writer.close();
 
         } catch (IOException e) {
             try {
                 FileWriter log = new FileWriter(logName, true);
                 try {
-                    log.write(e.getMessage());
+                    log.write(e.getMessage() + "\n");
                 } finally {
                     log.close();
                 }
@@ -44,6 +49,13 @@ public class Coder {
             }
         }
     }
+
+    public static void main(String[] args) {
+        char[] et = new char[]{'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '"'};
+//        System.out.println((int)et[10]);
+        codeFile("E:\\Java\\Education\\src\\ru\\progwards\\java1\\lessons\\Training\\tmp.txt", "E:\\Java\\Education\\src\\ru\\progwards\\java1\\lessons\\Training\\tmpout.txt", et, "E:\\Java\\Education\\src\\ru\\progwards\\java1\\lessons\\Training\\tmplog.txt");
+    }
+
 }
 
 
