8f1b6ed295bdb719b51c7bd13877b3173de6333f
liskotlas <lis_kotlas@mail.ru>
2019-12-24 19:47:21 +0300

    3

--- src/ru/progwards/java1/lessons/io2/Censor.java
+++ src/ru/progwards/java1/lessons/io2/Censor.java
@@ -14,26 +14,30 @@ public class Censor {
 
         try (FileReader fileReader = new FileReader(inoutFileName)) {
             Scanner scanner = new Scanner(fileReader);
-            while (scanner.hasNextLine()) {                     //разбитие на строки
-                String string = scanner.nextLine();
-                for (String word : string.split(" ")) {  //разбитие строки на слова
-                    for (char c : word.toCharArray()) {         //делаем слово чистым
-                        if (Character.isAlphabetic(c)) {
-                            reWord += c;
+            try {
+                while (scanner.hasNextLine()) {                     //разбитие на строки
+                    String string = scanner.nextLine();
+                    for (String word : string.split(" ")) {  //разбитие строки на слова
+                        for (char c : word.toCharArray()) {         //делаем слово чистым
+                            if (Character.isAlphabetic(c)) {
+                                reWord += c;
+                            }
                         }
-                    }
 
-                    for (String arr : obscene) {              //поиск слова в массиве
-                        if (arr.compareTo(reWord) == 0) {
-                            for (char c : reWord.toCharArray())
-                                word = word.replace(String.valueOf(c), "*");    // замена символов слова на символы *
-                            break;
+                        for (String arr : obscene) {              //поиск слова в массиве
+                            if (arr.compareTo(reWord) == 0) {
+                                for (char c : reWord.toCharArray())
+                                    word = word.replace(String.valueOf(c), "*");    // замена символов слова на символы *
+                                break;
+                            }
                         }
+                        stringOut += word + " ";
+                        reWord = "";
                     }
-                    stringOut += word + " ";
-                    reWord = "";
+                    stringOut = stringOut.trim();       //формируем строку вывода
                 }
-                stringOut = stringOut.trim();       //формируем строку вывода
+            }catch (Exception e){
+                throw new CensorException(e.getMessage(), inoutFileName);
             }
             try (FileWriter fileWriter = new FileWriter(inoutFileName)) {
                 fileWriter.write(stringOut);
@@ -50,11 +54,12 @@ public class Censor {
     }
 
     public static void main(String[] args) {
-        String[] obscene = new String[]{"Java", "Oracle", "Sun", "Microsystems", "puck"};
+//        String[] obscene = new String[]{"Java", "Oracle", "Sun", "Microsystems", "puck"};
+        String[] obscene = null;
         try {
-            censorFile("E:\\\\Education\\src\\ru\\progwards\\java1\\lessons\\io2\\Censor", obscene);
+            censorFile("E:\\Java\\Education\\src\\ru\\progwards\\java1\\lessons\\io2\\Censor", obscene);
         }catch (CensorException e){
-            System.out.println(e);
+            System.out.println(e.toString());
         }
 
     }
