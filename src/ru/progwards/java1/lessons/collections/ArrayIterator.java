b4493791c702f0a4a67fde03c7bb8f0c0b426ae1
liskotlas <lis_kotlas@mail.ru>
2019-12-25 20:36:42 +0300

    3

--- src/ru/progwards/java1/lessons/collections/ArrayIterator.java
+++ src/ru/progwards/java1/lessons/collections/ArrayIterator.java
@@ -1,50 +1,34 @@
 package ru.progwards.java1.lessons.collections;
 
-import java.util.ArrayList;
-import java.util.Collection;
 import java.util.Iterator;
-import java.util.List;
 
 public class ArrayIterator<T> implements Iterator<T> {
-    private static final Object T = null;
     private T[] array;
-    public int y;
+    private int indexArr;
 
     ArrayIterator(T[] array) {
         this.array = array;
-        y = -1;
+        int indexArr;
     }
-
-    ArrayIterator(T[] array, int y) {
-        this.array = array;
-        this.y = y;
-    }
-
 
     @Override
     public boolean hasNext() {
-        return array[y + 1]!= null ? true : false;
+        return (indexArr + 1) < array.length && array[indexArr + 1] != null ? true : false;
     }
 
     @Override
     public T next() {
-            return array[y + 1]!= null ? array[y + 1] : null;
+        ++indexArr;
+        System.out.println(indexArr);
+        return array[indexArr] != null ? array[indexArr] : null;
     }
 
     public static void main(String[] args) {
-        ArrayList <Integer> integerList = new ArrayList<>();
-        Iterator <Integer> iterator = integerList.iterator();
-        integerList.add(0);
-        integerList.add(1);
-        integerList.add(2);
-        integerList.add(3);
-        integerList.add(4);
-        integerList.add(5);
+        Integer[] arr = {0, 1, 2, 3, 4};
+        ArrayIterator<Integer> iterator1 = new ArrayIterator<>(arr);
+        while (iterator1.hasNext()) {
 
-        for (;iterator.hasNext();){
-
-
+            System.out.println(iterator1.next());
         }
-
     }
 }
---
+++ src/ru/progwards/java1/lessons/collections/MatrixIterator.java
@@ -0,0 +1,41 @@
+package ru.progwards.java1.lessons.collections;
+
+
+import java.util.Iterator;
+
+public class MatrixIterator<T> implements Iterator<T> {
+    private T[][] array;
+    private int indexArrX;
+    private int indexArrY;
+
+    MatrixIterator(T[][] array) {
+        this.array = array;
+        indexArrX = -1;
+        indexArrY = -1;
+    }
+
+    @Override
+    public boolean hasNext() {
+        if (indexArrX >= array[indexArrY].length) {
+            ++indexArrY;
+            indexArrX = -1;
+            return (indexArrX + 1) < array[indexArrY].length && (indexArrY + 1) < array.length && array[indexArrX + 1] != null ? true : false;
+        }
+        return false;
+    }
+
+    @Override
+    public T next() {
+        indexArrX++;
+
+        return array[indexArrY][indexArrX] != null ? array[indexArrY][indexArrX] : null;
+    }
+
+    public static void main(String[] args) {
+        Integer [][] matrix = {{0, 1,2,3,4},{10,11,12,13,14}};
+        ArrayIterator <Integer> arrayIterator = new ArrayIterator<Integer>(matrix);
+        while (arrayIterator.hasNext()){
+            System.out.println(arrayIterator.next());
+        }
+    }
+}
