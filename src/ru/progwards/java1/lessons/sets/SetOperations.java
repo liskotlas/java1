a77ca81b11da34c9f216195e19d64cc0a08242ca
liskotlas <lis_kotlas@mail.ru>
2020-01-02 22:21:18 +0300

    3

--- src/ru/progwards/java1/lessons/sets/SetOperations.java
+++ src/ru/progwards/java1/lessons/sets/SetOperations.java
@@ -1,6 +1,7 @@
 package ru.progwards.java1.lessons.sets;
 
 import java.util.Set;
+import java.util.TreeSet;
 
 public class SetOperations {
 
@@ -23,12 +24,45 @@ public class SetOperations {
 
     }
 
-    public static Set<Integer> symDifference(Set<Integer> set1, Set<Integer> set2){
-        Set<Integer>res1 = set1;
-        Set<Integer>res2 = set2;
-        res1.removeAll(set2);
-        res2.removeAll(set1);
-        res1.addAll(res2);
+    public static Set<Integer> symDifference(final Set<Integer> set1, final Set<Integer> set2){
+        Set<Integer>res1 =set1;
+        Set<Integer>res2 = new TreeSet<>();
+        for (Integer a: set2){
+            res2.add(a);
+        }
+
+        res2.retainAll(set1);
+
+        res1.addAll(set2);
+
+        res1.removeAll(res2);
         return res1;
+    }
+
+    public static void main(String[] args) {
+        Set <Integer> set1 = new TreeSet<>();
+        Set <Integer> set2 = new TreeSet<>();
+        set1.add(1);
+        set1.add(2);
+        set1.add(3);
+        set1.add(4);
+        set1.add(5);
+        set1.add(6);
+        set1.add(7);
+        set1.add(9);
+        set1.add(10);
+
+        set2.add(1);
+        set2.add(2);
+        set2.add(4);
+        set2.add(5);
+        set2.add(6);
+        set2.add(7);
+        set2.add(8);
+        set2.add(9);
+        set2.add(10);
+
+        System.out.println(symDifference(set1, set2).toString());
+
     }
 }
