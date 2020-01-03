b2006620948de1f1a8af3c9a95a0ba150cc0c6dc
liskotlas <lis_kotlas@mail.ru>
2020-01-03 19:57:10 +0300

    3

--- src/ru/progwards/java1/lessons/helloworld/Task2.java
+++ src/ru/progwards/java1/lessons/helloworld/Task2.java
@@ -5,8 +5,8 @@ public class Task2 {
     public static void subtraction(int x, int y) {
         int z;
         z = x + y;
-        System.out.println("Вызвана функция subtraction() с параметрами x = " + x + ", y = " + y);
-        System.out.print("Результат z = " + z);
+        System.out.println("Вызвана функция subtraction() с параметрами x =" + x + ", y =" + y);
+        System.out.print("Результат z =" + z);
     }
 
     public static void main(String[] args) {
