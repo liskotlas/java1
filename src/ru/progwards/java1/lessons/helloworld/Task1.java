8ee47deeb900ad69827fc2fd8c14e9b64b848d26
liskotlas <lis_kotlas@mail.ru>
2020-01-03 19:31:49 +0300

    3

---
+++ src/ru/progwards/java1/lessons/helloworld/Task1.java
@@ -0,0 +1,18 @@
+
+package ru.progwards.java1.lessons.helloworld;
+
+public class Task1 {
+    public static void main(String[] args) {
+        String one = "Хорошо идут дела";
+        String two = "Изучаю Java я!";
+        String three = " ";
+        System.out.println(one);
+        System.out.println(two);
+        System.out.print(one);
+        System.out.print(three);
+        System.out.println(two);
+        System.out.print(two);
+        System.out.print(three);
+        System.out.println(one);
+    }
+}
