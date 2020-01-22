228e93512f016a359c9f63d104df67a4f77f19ac
liskotlas <lis_kotlas@mail.ru>
2020-01-22 22:13:04 +0300

    3

--- src/ru/progwards/java1/lessons/files/FindDuplicates.java
+++ src/ru/progwards/java1/lessons/files/FindDuplicates.java
@@ -11,68 +11,75 @@ import java.util.*;
 
 public class FindDuplicates {
 
-//    Создание Map с именами файлов по результатам сравнения файлов
-    private Map <Path, ArrayList <Path>> fileList (String start) throws IOException{
+    //    Создание Map с именами файлов по результатам сравнения файлов
+    private Map<Path, ArrayList<Path>> fileList(String start){
         Map<Path, ArrayList<Path>> fileList = new HashMap<>();
         Path pathFile = Paths.get(start);
         PathMatcher pathMatcher = FileSystems.getDefault().getPathMatcher("glob:**/*");
-        Files.walkFileTree(pathFile, new SimpleFileVisitor<Path>(){
-            @Override
-            public FileVisitResult visitFile(Path path, BasicFileAttributes attrs) throws IOException {
-                if (pathMatcher.matches(pathFile.relativize(path))) {
-                    if (fileList.containsKey(path.getFileName()) && fullFiltr(fileList.get(path.getFileName()).get(0), path)) {
+        try {
+            Files.walkFileTree(pathFile, new SimpleFileVisitor<Path>() {
+
+                @Override
+                public FileVisitResult visitFile(Path path, BasicFileAttributes attrs) throws IOException {
+                    if (pathMatcher.matches(pathFile.relativize(path))) {
+                        if (fileList.containsKey(path.getFileName()) && fullFiltr(fileList.get(path.getFileName()).get(0), path)) {
                             fileList.get(path.getFileName()).add(path);
                             return FileVisitResult.CONTINUE;
+                        }
+                        ArrayList<Path> tmpPath = new ArrayList<>();
+                        tmpPath.add(path);
+                        fileList.putIfAbsent(path.getFileName(), tmpPath);
+                        return FileVisitResult.CONTINUE;
                     }
-                    ArrayList<Path> tmpPath = new ArrayList<>();
-                    tmpPath.add(path);
-                    fileList.putIfAbsent(path.getFileName(), tmpPath);
                     return FileVisitResult.CONTINUE;
-
                 }
-                return FileVisitResult.CONTINUE;
-            }
 
-            @Override
-            public FileVisitResult visitFileFailed(Path file, IOException exc){
-                return FileVisitResult.CONTINUE;
-            }
-        });
+                @Override
+                public FileVisitResult visitFileFailed(Path file, IOException exc) {
+                    return FileVisitResult.CONTINUE;
+                }
+            });
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         return fileList;
     }
 
 
-// сравнение файлов по атрибутам и содержимому
-    private boolean fullFiltr (Path file1, Path file2)throws IOException{
-        if (Files.getAttribute(file1, "lastModifiedTime") == Files.getAttribute(file2, "lastModifiedTime") && Files.getAttribute(file1, "size") == Files.getAttribute(file2, "size")){
-            if (Arrays.equals(Files.readAllBytes(file1), Files.readAllBytes(file2))){
-                return true;
+    // сравнение файлов по атрибутам и содержимому
+    private boolean fullFiltr(Path file1, Path file2){
+        try {
+            if (Files.getAttribute(file1, "lastModifiedTime") == Files.getAttribute(file2, "lastModifiedTime") && Files.getAttribute(file1, "size") == Files.getAttribute(file2, "size")) {
+                if (Arrays.equals(Files.readAllBytes(file1), Files.readAllBytes(file2))) {
+                    return true;
+                }
             }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
         return false;
     }
 
-//    обработка Map и вывод результата
-    public List<List<String>> findDuplicates(String startPath) throws IOException{
-        List <List<String>> result = new ArrayList<>();
-        for (var entry : fileList(startPath).entrySet()){
-            if (entry.getValue().size()>0){
+    //    обработка Map и вывод результата
+    public List<List<String>> findDuplicates(String startPath){
+        List<List<String>> result = new ArrayList<>();
+        for (var entry : fileList(startPath).entrySet()) {
+            if (entry.getValue().size() > 0) {
                 List<String> stringPath = new ArrayList<>();
-                for (Path path : entry.getValue()){
+                for (Path path : entry.getValue()) {
                     stringPath.add(path.toString());
                 }
                 result.add(stringPath);
             }
         }
         return result;
-        }
+    }
 
 
-
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args){
         FindDuplicates findDuplicates = new FindDuplicates();
-        for (var pass : findDuplicates.findDuplicates("e:/Java/Education/src/ru/progwards/java1/lessons")){
-            for (String s : pass){
+        for (var pass : findDuplicates.findDuplicates("e:/Java/Education/src/ru/progwards/java1/lessons")) {
+            for (String s : pass) {
                 System.out.println(s);
             }
 //            System.out.println(entry.getValue().getClass());
